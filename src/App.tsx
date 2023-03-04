2052f7e78ad885ba2af72ad9c263f00f5cad90b7
jarenadams21 <jarenadams3@gmail.com>
2023-03-04 10:18:16 -0500

    tsconfig + functional component creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './components/Header';
+import Main from './components/Main';
 import './App.css';
+import User from './components/User';
+import AddNewTodo from './components/AddNewTodo';
+import Calendar from './components/Calendar';
+import Projects from './components/Projects';
+import Todos from './components/Todos';
+import EditTodo from './components/EditTodo';
+
+
 
 function App() {
   return (
     <div className="App">
       <Header>
-
+        <User/>
+        <AddNewTodo/>
+        <Calendar/>
+        <Projects />
       </Header>
       <Main>
-        
+          <Todos/>
+          <EditTodo/>
       </Main>
     </div>
   );
